4de9ca30b6a657bff063644fceeccee6a397e2a3
Michel Junio <micheljunioferreira@gmail.com>
2022-11-17 15:11:36 -0300

    ajuste

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">Ainda não há itens! Adicione um acima!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
